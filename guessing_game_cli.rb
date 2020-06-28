0f218d5f6ecfb6f0caecbc57638698d23eb5ab88
Andrew Park <acpark123@gmail.com>
2020-06-28 04:45:04 +0000

    Automatically backed up by Learn

--- guessing_game_cli.rb
+++ guessing_game_cli.rb
@@ -4,6 +4,14 @@ def random_number
   random_number = rand(6) + 1
 end
 
+def prompt
+  puts "Guess a number between 1 and 6"
+end
+
+def capture
+  user_input = gets.chomp
+end
+
 def run_guessing_game
   
   
