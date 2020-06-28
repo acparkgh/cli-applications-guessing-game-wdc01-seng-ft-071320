eb6f391629d9fc2bd40962a765d73adfbfd00832
Andrew Park <acpark123@gmail.com>
2020-06-28 04:48:19 +0000

    Automatically backed up by Learn

--- guessing_game_cli.rb
+++ guessing_game_cli.rb
@@ -14,8 +14,15 @@ end
 
 def run_guessing_game
   
+  random_number
+  prompt
+  capture
   
-  
-  
-  
+    if user_input == random_number
+      puts "You guessed the correct number!"
+    elsif user_input == "exit"
+      puts "Goodbye!"
+    else
+      puts "Sorry!, The computer guessed #{random_number}."
+    end
 end
